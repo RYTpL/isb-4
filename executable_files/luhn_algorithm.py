938a915c92e5bb8c344aee75c102df06ddc351a1
Egor Sysolyatin <113940124+RYTpL@users.noreply.github.com>
2023-05-29 22:34:01 +0400

    added a check for the correctness of entering the card number

--- executable_files/luhn_algorithm.py
+++ executable_files/luhn_algorithm.py
@@ -1,1 +1,10 @@
 def luhn_algorithm(card_number:str)->bool:
+    """
+    Func that checks the correctness of the card number
+    """
+    digits = list(map(int, str(card_number)))[::-1]
+    for i in range(1, len(digits), 2):
+        digits[i]*=2
+        if digits[i]>9:
+            digits[i] = digits[i] // 10 + digits[i] % 10
+    return sum(digits) % 10 == 0 
--- executable_files/menu.py
+++ executable_files/menu.py
@@ -3,6 +3,6 @@ import time
 import logging
 
 from executable_files.card_info import 
-from executable_files.luhn_algorithm import 
+from executable_files.luhn_algorithm import luhn_algorithm
 from executable_files.card_number import 
 from executable_files.cores_time import 
